9eb8495370cc498cb548f485715752f4272e5ae8
Alex Butler <a.jbutler019@gmail.com>
2016-02-29 00:02:56 +1000

    Separated auth routes into its own controller.

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,7 +1,6 @@
 package controllers
 
 import "github.com/revel/revel"
-import "qixalite.com/Ranndom/ldap-portal/app"
 
 type App struct {
     *revel.Controller
@@ -9,17 +8,6 @@ type App struct {
 
 func (c App) Index() revel.Result {
     return c.Render()
-}
-
-func (c App) Login() revel.Result {
-    return c.Render()
-}
-
-func (c App) Logout() revel.Result {
-    delete(c.Session, "user")
-    c.Flash.Success("Successfully logged out!")
-
-    return c.Redirect(App.Index)
 }
 
 func (c App) Account() revel.Result {
@@ -39,28 +27,5 @@ func (c App) POST_Account(givenName string, surname string) revel.Result {
     c.Flash.Success("Updated account!")
 
     return c.Redirect(App.Account)
-}
-
-func (c App) POST_Login(username string, password string) revel.Result {
-    c.Validation.Required(username).Message("Please enter a username")
-    c.Validation.Required(password).Message("Please enter a password")
-
-    if c.Validation.HasErrors() {
-        c.Validation.Keep()
-        c.FlashParams()
-        return c.Redirect(App.Login)
-    }
-
-    u, status := app.GetLDAPUser(username)
-    if status == false {
-        // User could not be retrieved.
-        c.Flash.Error("Invalid username/password")
-        return c.Redirect(App.Login)
-    }
-
-    c.Session["user"] = u.UID
-    c.Flash.Success("Welcome, " + u.UID)
-
-    return c.Redirect(App.Index)
 }
 
