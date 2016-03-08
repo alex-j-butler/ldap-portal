68ac43c145d50509ccc4670b54950798c0040686
Alex Butler <a.jbutler019@gmail.com>
2016-03-08 16:28:06 +1000

    Added misc helpers

---
+++ modules/helpers/misc.go
@@ -0,0 +1,13 @@
+package helpers
+
+import (
+    "gopkg.in/macaron.v1"
+    "github.com/go-macaron/session"
+    "qixalite.com/Ranndom/ldap-portal/modules/database"
+    "qixalite.com/Ranndom/ldap-portal/models"
+)
+
+func CheckExists(ctx *macaron.Context, id int) (int) {
+    return 404
+}
+
