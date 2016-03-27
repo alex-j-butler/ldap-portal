b1fde5f265fb07dcf95d0a34e5cb4be8990f7e95
Alex Butler <a.jbutler019@gmail.com>
2016-03-27 16:41:13 +1000

    Added missing maps helpers.

---
+++ src/qixalite.com/Ranndom/ldap-portal/modules/helpers/maps.go
@@ -0,0 +1,37 @@
+package helpers
+
+import (
+    "reflect"
+)
+
+func MapIndex(vs []string, t string) int {
+    for i, v := range vs {
+        if v == t {
+            return i
+        }
+    }
+
+    return -1
+}
+
+func MapInclude(vs []string, t string) bool {
+    return MapIndex(vs, t) >= 0
+}
+
+func MapWhitelist(m *map[string]interface{}, whitelist []string) {
+    for k, _ := range *m {
+        if !MapInclude(whitelist, k) {
+            delete(*m, k)
+        }
+    }
+}
+
+func UpdateStruct(s interface{}, m *map[string]interface{}) {
+    reflectStruct := reflect.ValueOf(&s).Elem()
+
+    for k, v := range *m {
+        val := reflect.ValueOf(v)
+        reflectStruct.FieldByName(k).Set(val)
+    }
+}
+
