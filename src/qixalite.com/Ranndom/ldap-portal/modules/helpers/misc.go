b74828612f0eb74b9f45bb3453302b8281b54936
Alex Butler <a.jbutler019@gmail.com>
2016-03-30 10:16:56 +1000

    Added named formatting helper.

--- src/qixalite.com/Ranndom/ldap-portal/modules/helpers/misc.go
+++ src/qixalite.com/Ranndom/ldap-portal/modules/helpers/misc.go
@@ -2,9 +2,24 @@ package helpers
 
 import (
 	"gopkg.in/macaron.v1"
+	"bytes"
+	"text/template"
 )
 
 func CheckExists(ctx *macaron.Context, id int) (int) {
 	return 404
+}
+
+func Format(format string, context map[string]interface{}) string {
+	var text bytes.Buffer
+	textTemplate := template.New("formatting_template")
+	if _, err := textTemplate.Parse(format); err != nil {
+		return ""
+	}
+	if err := textTemplate.Execute(&text, context); err != nil {
+		return ""
+	}
+
+	return text.String()
 }
 
